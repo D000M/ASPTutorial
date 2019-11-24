1000999c9c460389a08d85dcdc8536c437ed76e7
D000M <georgislawow@gmail.com>
2019-11-24 14:53:05 +0200

    Initial commit
    Added all files except packages

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KrusovaASPV2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [Authorize]
+        public ActionResult LoginIndex()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kurs1()
+        {
+            return View();
+        }
+
+        public ActionResult Kurs2()
+        {
+            return View();
+        }
+
+        public ActionResult Kurs3()
+        {
+            return View();
+        }
+        public ActionResult Kurs4()
+        {
+            return View();
+        }
+        public ActionResult Unibit()
+        {
+            return View();
+        }
+        public ActionResult Kurs1Schedule()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Mail;
+using System.Net;
+using KrusovaASPV2.Models;
+using System.Web.Security;
+
+namespace KrusovaASPV2.Controllers
+{
+    public class UserController : Controller
+    {
+        //Registration Action
+        [HttpGet]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        
+        //Registration POST Action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registration( [Bind(Exclude = "IsEmailVerified,ActivationCode")] User user)
+        {
+
+            bool Status = false;
+            string Message = "";
+
+            //Model validation
+            if (ModelState.IsValid)
+            {
+
+                var isExist = IsEmailExist(user.EmailID);
+                if (isExist)
+                {
+                    ModelState.AddModelError("EmailExist", "Email already exist");
+                    return View(user);
+                }
+
+                //Generate Activition Code
+                user.ActivationCode = Guid.NewGuid();
+
+                //Password Hashing
+                user.Password = Crypto.Hash(user.Password);
+                user.ConfirmPassword = Crypto.Hash(user.ConfirmPassword); //
+
+                user.IsEmailVerified = false;
+
+                //Save data to Database
+                using (MyDatabaseEntities dc = new MyDatabaseEntities())
+                {
+                    dc.Users.Add(user);
+                    dc.SaveChanges();
+
+                    //Send Email to User
+                    SendVerificationLinkEmail(user.EmailID, user.ActivationCode.ToString());
+                    Message = "Registration successfully done. Account activation link has been send to your email: " + user.EmailID;
+                    Status = true;
+                }
+            }
+            else
+            {
+                Message = "Invalid Request";
+            }
+
+            ViewBag.Message = Message;
+            ViewBag.Status = Status;
+            
+            return View(user);
+        }
+        
+        //Verify Account
+        [HttpGet]
+        public ActionResult VerifyAccount(string id)
+        {
+            bool Status = false;
+
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                //  This line i have added here to avoid confirm password does not match issue on save changes
+                dc.Configuration.ValidateOnSaveEnabled = false;
+
+                var v = dc.Users.Where(a => a.ActivationCode == new Guid(id)).FirstOrDefault();
+                if(v != null)
+                {
+                    v.IsEmailVerified = true;
+                    dc.SaveChanges();
+                    Status = true;
+                }
+                else
+                {
+                    ViewBag.Message = "Invalid Request";
+                }
+            }
+            ViewBag.Status = Status;
+            return View();
+        }
+
+        //Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        //Login POST
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(UserLogin login, string ReturnUrl)
+        {
+            string message = "";
+
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == login.EmailID).FirstOrDefault();
+                if(v != null)
+                {
+                    if(string.Compare(Crypto.Hash(login.Password), v.Password) == 0)    //Valid Password
+                    {
+                        int timeout = login.RememberMe ? 525600 : 20;    // first arg = 1 year login , second arg = 20 mins
+                        var ticket = new FormsAuthenticationTicket(login.EmailID, login.RememberMe, timeout);
+                        string encrypted = FormsAuthentication.Encrypt(ticket);
+                        var cookie = new HttpCookie(FormsAuthentication.FormsCookieName, encrypted);
+                        cookie.Expires = DateTime.Now.AddMinutes(timeout);
+                        cookie.HttpOnly = true;
+                        Response.Cookies.Add(cookie);
+
+                        if(Url.IsLocalUrl(ReturnUrl))
+                        {
+                            return Redirect(ReturnUrl);
+                        }
+                        else
+                        {
+                            return RedirectToAction("LoginIndex", "Home");
+                        }
+                    }
+                    else
+                    {
+                        message = "Invalid credential provided";
+                    }
+                }
+                else
+                {
+                    message = "Invalid credential provided";
+                }
+            }
+
+            ViewBag.Message = message;
+
+            return View();
+        }
+        
+        //Logout
+        [Authorize]
+        [HttpPost]
+        public ActionResult Logout()
+        {
+
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Login", "User");
+        }
+        
+
+
+        [NonAction]
+        public bool IsEmailExist(string emailID)
+        {
+            using (MyDatabaseEntities dc = new MyDatabaseEntities())
+            {
+                var v = dc.Users.Where(a => a.EmailID == emailID).FirstOrDefault();
+                return v != null;
+            }
+        }
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID, string activationCode)
+        {
+            var verifyUrl = "/User/VerifyAccount/" + activationCode;
+            var link = Request.Url.AbsoluteUri.Replace(Request.Url.PathAndQuery, verifyUrl);
+
+            //Set Gmail settings for this account
+            // https://accounts.google.com/DisplayUnlockCaptcha
+            // https://www.google.com/settings/security/lesssecureapps
+            //set the site owner email addres here
+            var fromEmail = new MailAddress("tashinkosapa222@gmail.com", "Test Register");
+            var toEmail = new MailAddress(emailID);
+            //set the site owner email adress password here
+            var fromEmailPassword = "sportCAFE1!";    //Replace with actual password
+            string subject = "Your account is successfully created!";
+            string body = "<br/><br/>Your account is Successfully created." +
+                " Please click on the below link to verify your account<br/><br/><a href='" + link + "'>" + link + "</a>";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+            };
+
+            using (var Message = new MailMessage(fromEmail, toEmail)
+            {
+                Subject = subject,
+                Body = body,
+                IsBodyHtml = true
+            })
+
+            smtp.Send(Message);
+        }
+    }   //end of the class
+
+    
+}
